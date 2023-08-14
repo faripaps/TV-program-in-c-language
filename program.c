12a86041b8f9479402975b3fb44fb62aac2989e9
George Papaya <38511993+faripaps@users.noreply.github.com>
2023-08-14 11:49:30 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_CHANNELS 10
 
@@ -12,13 +14,13 @@ struct Channel {
 void displayChannelGuide(struct Channel channels[], int numChannels)
 {
     printf("TV Channel Guide:\n");
-    printf("-------------------------------------------------\n");
-    printf("Channel Number\t| Channel Name\n");
-    printf("-------------------------------------------------\n");
+    printf("----------------------------------\n");
+    printf("| Channel Number | Channel Name  |\n");
+    printf("----------------------------------\n");
     for (int i = 0; i < numChannels; i++) {
-        printf("%d\t\t| %s\n", channels[i].number, channels[i].name);
+        printf("| %-14d | %-12s |\n", channels[i].number, channels[i].name);
     }
-    printf("-------------------------------------------------\n");
+    printf("----------------------------------\n");
 }
 
 int main()
@@ -27,16 +29,14 @@ int main()
 
     // Adding TV channels to the guide
     channels[0].number = 1;
-    strcpy(channels[0].name, "CBS");
+    strcpy(channels[0].name, "ZBC");
     channels[1].number = 2;
-    strcpy(channels[1].name, "NBC");
+    strcpy(channels[1].name, "Fx News");
     channels[2].number = 3;
-    strcpy(channels[2].name, "ABC");
+    strcpy(channels[2].name, "supersport 3");
     channels[3].number = 4;
-    strcpy(channels[3].name, "FOX");
-    channels[4].number = 5;
-    strcpy(channels[4].name, "ESPN");
-    int numChannels = 5;
+    strcpy(channels[3].name, "ESPN");
+    int numChannels = 4;
 
     // Display the TV channel guide
     displayChannelGuide(channels, numChannels);
